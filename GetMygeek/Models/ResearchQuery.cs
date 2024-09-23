51590363462bac4f6bafd2b44bc3cc1bb9515480
1J0K3R <giroud.joshua69@gmail.com>
2024-09-23 16:06:51 +0200

    Tout est OK, on est des GOATTTTT

--- GetMygeek/Models/ResearchQuery.cs
+++ GetMygeek/Models/ResearchQuery.cs
@@ -1,8 +1,19 @@
 ﻿namespace GetMygeek.Models;
-    public class ResearchQuery
+public class ResearchQuery
+{
+    public string input { get; set; } = null;
+    public string available { get; set; } = null;
+    public int? anciennete { get; set; } = null;
+    public IEnumerable<string> profilesSelected { get; set; } = new HashSet<string>() { };
+
+    public bool IsImplement()
     {
-        public string input { get; set; }
-        public string available { get; set; }
-        public int anciennete { get; set; }
-        public IEnumerable<string> profilesSelected { get; set; } = new HashSet<string>() { };
+        if (input is not null || available is not null || anciennete is not null || profilesSelected.Count()>0)
+        {
+            return true;
+        }else 
+        {
+            return false;
+        }
     }
+}
