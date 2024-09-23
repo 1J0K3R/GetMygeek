e1e8248d3257aa809302ae3e74969bc6644641bb
1J0K3R <giroud.joshua69@gmail.com>
2024-09-23 15:27:14 +0200

    il faut patch la researchQuery passer en paramètre de phototèque

---
+++ GetMygeek/Models/ResearchQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace GetMygeek.Models;
+    public class ResearchQuery
+    {
+        public string input { get; set; }
+        public string available { get; set; }
+        public int anciennete { get; set; }
+        public IEnumerable<string> profilesSelected { get; set; } = new HashSet<string>() { };
+    }
