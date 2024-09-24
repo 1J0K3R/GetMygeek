0a899d4345d1525cf708becd1991d3e35367f109
1J0K3R <giroud.joshua69@gmail.com>
2024-09-24 11:13:59 +0200

    Création du popup Missions

--- GetMygeek/Components/Pages/Card.razor.cs
+++ GetMygeek/Components/Pages/Card.razor.cs
@@ -2,25 +2,26 @@
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
-namespace GetMygeek.Components.Pages
+namespace GetMygeek.Components.Pages;
+public partial class Card
 {
-    public partial class Card
+    /// <summary>
+    /// Objet Profil qui contient toutes les informations d'un consultant 
+    /// </summary
+    [Parameter]
+    public Phototeque Profil { get; set; }
+
+    /// <summary>
+    /// Popup qui récupère et affiche la mission du consultant
+    /// </summary>
+    private MissionPopup missionPopup;
+
+    /// <summary>
+    /// Cette fonction permet d'afficher la popup qui contient la mission en cours d'un consultant
+    /// </summary>
+    /// <returns></returns>
+    public async Task OpenMissionInformationAsync()
     {
-        [Parameter]
-        public Phototeque Profil { get; set; }
-
-        #region DDC Message Box
-        public MudMessageBox _mudMessageBox;
-
-
-        /// <summary>
-        /// Cette fonction permet d'afficher la fenetre MudMessage qui contient le DDC [Dossier De Compétence]
-        /// </summary>
-        /// <returns></returns>
-        public async Task OpenDDCInformationAsync()
-        {
-            bool? result = await _mudMessageBox.ShowAsync();
-        }
-        #endregion
+        await missionPopup.ShowAsync();
     }
 }
---
+++ GetMygeek/Models/Mission.cs
@@ -0,0 +1,28 @@
+﻿namespace GetMygeek.Models;
+public class Mission
+{
+    /// <summary>
+    /// Id de la mission
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Id du consultant de la mission
+    /// </summary>
+    public string IdConsultant { get; set; }
+    
+    /// <summary>
+    /// Entreprise au sein de laquelle est effectuée la mission
+    /// </summary>
+    public string Entreprise { get; set; }
+    
+    /// <summary>
+    /// Ingénieure d'affaire responsable de la mission 
+    /// </summary>
+    public string ResponsableMission { get; set; }
+    
+    /// <summary>
+    /// Fiche de poste de la mission 
+    /// </summary>
+    public string FicheDePoste { get; set; }
+}
